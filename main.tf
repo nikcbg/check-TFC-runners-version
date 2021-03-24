c859a785a4306581d693a79e08c2fb283899c319
nikcbg <nikcbg@yahoo.com>
2021-03-24 08:09:43 +0200

    Update main.tf

--- main.tf
+++ main.tf
@@ -10,3 +10,16 @@ resource "null_resource" "release" {
     command = "free -h"
   }
 }
+
+resource "null_resource" "release2" {
+  triggers = {
+    always_run = timestamp()
+  }
+  
+  provisioner "local-exec" {
+    command = "cat /etc/*release"
+  }
+  provisioner "local-exec" {
+    command = "free -h"
+  }
+}
