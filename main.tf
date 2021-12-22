00875fbaeb42f2d608db030b5c2e35579425f1ec
nikcbg <nikcbg@yahoo.com>
2021-12-22 10:00:23 +0200

    Update main.tf

--- main.tf
+++ main.tf
@@ -11,7 +11,7 @@ resource "null_resource" "release" {
   }
   provisioner "local-exec" {
     #command = "python --version"
-    command = "python3 -V; pip3 --version;python3 -m pip install --upgrade pip" 
+    command = "python3 -m install --upgrade python3.9;python3 -V" 
   }
 
 }
